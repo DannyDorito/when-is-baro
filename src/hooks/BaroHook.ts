724e0312856bbbfedeb1a3734d06078253f062b1
John Allison <john@jallison.co.uk>
2025-07-12 14:12:00 +0100

    Update BaroHook.ts

--- src/hooks/BaroHook.ts
+++ src/hooks/BaroHook.ts
@@ -1,48 +1,54 @@
 import { useState, useEffect } from "react";
 import type { BaroData } from "../interfaces/BaroData";
 
+const getBaroData = async (
+  setBaroData: React.Dispatch<React.SetStateAction<BaroData | undefined>>,
+  signal: AbortSignal
+) => {
+  try {
+    const response = await fetch(
+      "https://corsproxy.io/?https://content.warframe.com/dynamic/worldState.php",
+      { signal }
+    );
+    const data = await response.json();
+    const voidTraders = data.VoidTraders;
+    const baro = voidTraders[0];
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const getTimestamp = (dateObj: any) => {
+      if (
+        typeof dateObj === "object" &&
+        dateObj.$date &&
+        dateObj.$date.$numberLong
+      ) {
+        return parseInt(dateObj.$date.$numberLong, 10);
+      }
+      return dateObj;
+    };
+
+    const relay = baro.Node.replace("HUB", " Relay");
+    const arrival = new Date(getTimestamp(baro.Activation));
+    const departure = new Date(getTimestamp(baro.Expiry));
+
+    setBaroData({
+      relay,
+      arrival,
+      departure: departure,
+    });
+  } catch (error) {
+    console.error(error); //TODO: Log this error properly
+  }
+};
+
 export function useBaro() {
   const [baroData, setBaroData] = useState<BaroData | undefined>(undefined);
   useEffect(() => {
-    const fetchData = async () => {
-      await getBaroData();
+    const controller = new AbortController();
+    getBaroData(setBaroData, controller.signal);
+    return () => {
+      controller.abort();
     };
-    fetchData();
   }, []);
-
-  const getBaroData = async () => {
-    fetch(
-      "https://corsproxy.io/?https://content.warframe.com/dynamic/worldState.php"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const voidTraders = data.VoidTraders;
-        const baro = voidTraders[0];
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const getTimestamp = (dateObj: any) => {
-          if (
-            typeof dateObj === "object" &&
-            dateObj.$date &&
-            dateObj.$date.$numberLong
-          ) {
-            return parseInt(dateObj.$date.$numberLong, 10);
-          }
-          return dateObj;
-        };
-
-        const relay = baro.Node.replace("HUB", " Relay");
-        const arrival = new Date(getTimestamp(baro.Activation));
-        const departure = new Date(getTimestamp(baro.Expiry));
-
-        setBaroData({
-          relay,
-          arrival,
-          departure: departure,
-        });
-      })
-      .catch((error) => console.error(error));
-  };
 
   return baroData;
 }
